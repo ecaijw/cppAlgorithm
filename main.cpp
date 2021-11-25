1d9ae07f44316d61451af63528a8a21f4de2dabb
caijingwei <ecaijw@msn.com>
2021-11-26 02:41:33 +0800

    optimize: calc 24

--- main.cpp
+++ main.cpp
@@ -423,7 +423,15 @@ public:
         cout << "calc exp: " << ret << endl;
     }
 
-    bool calc24(const vector<int>& cards) {
+    bool calc24_1(const vector<int>& cards) {
+        vector< vector<int> > pos;
+        int cardPos [] = {0, 1, 2, 3};
+        do {
+            vector<int> onePos;
+            onePos.assign(cardPos, cardPos + 4);
+            pos.push_back(onePos);
+        } while (next_permutation(cardPos, cardPos + cards.size()));
+
         // exp: (3*(4)*(5)*6)
         //      0/12/345/67/8
         //      2442324222412
@@ -435,18 +443,18 @@ public:
         creator.getFirst(numArray);
         while (creator.getNext(numArray)) {
 //            Tools::print(numArray);
-            int cardPos [] = {0, 1, 2, 3};
-            do {
+            for (int iPos = 0; iPos < pos.size(); iPos++) {
+                vector<int>& onePos = pos.at(iPos);
                 string exp;
                 if (numArray[0] == 1) {
                     exp.push_back('(');
                 }
-                exp.push_back((char)(cards[cardPos[0]] + '0'));
+                exp.push_back((char)(cards[onePos[0]] + '0'));
                 exp.push_back(OPERATOR[numArray[1]]);
                 if (numArray[2] == 1) {
                     exp.push_back('(');
                 }
-                exp.push_back((char)(cards[cardPos[1]] + '0'));
+                exp.push_back((char)(cards[onePos[1]] + '0'));
                 if (numArray[3] == 1) {
                     exp.push_back(')');
                 }
@@ -454,12 +462,12 @@ public:
                 if (numArray[5] == 1) {
                     exp.push_back('(');
                 }
-                exp.push_back((char)(cards[cardPos[2]] + '0'));
+                exp.push_back((char)(cards[onePos[2]] + '0'));
                 if (numArray[6] == 1) {
                     exp.push_back(')');
                 }
                 exp.push_back(OPERATOR[numArray[7]]);
-                exp.push_back((char)(cards[cardPos[3]] + '0'));
+                exp.push_back((char)(cards[onePos[3]] + '0'));
                 if (numArray[8] == 1) {
                     exp.push_back(')');
                 }
@@ -476,11 +484,84 @@ public:
                     cout << "exp: " << exp << "   suffix: " << suffix << "  ret: " << ret << endl;
                     return true;
                 }
-            } while (next_permutation(cardPos, cardPos + cards.size()));
+            }
 
         }
         return false;
     }
+
+    bool calc24(const vector<int>& cards) {
+        vector< vector<int> > pos;
+        int cardPos [] = {0, 1, 2, 3};
+        do {
+            vector<int> onePos;
+            onePos.assign(cardPos, cardPos + 4);
+            pos.push_back(onePos);
+        } while (next_permutation(cardPos, cardPos + cards.size()));
+
+        // exp: (3*(4)*(5)*6)
+        //      0/12/345/67/8
+        //      2442324222412
+        //      数字4*3*2  *  运算符4*4*4  *  括号2*2*2*2*2*2
+        const vector<char> OPERATOR({'+', '-', '*', '/'});
+        for (int i0 = 0; i0 < 2;i0++)
+        for (int i1 = 0; i1 < 4;i1++)
+        for (int i2 = 0; i2 < 2;i2++)
+        for (int i3 = 0; i3 < 2;i3++)
+        for (int i4 = 0; i4 < 4;i4++)
+        for (int i5 = 0; i5 < 2;i5++)
+        for (int i6 = 0; i6 < 2;i6++)
+        for (int i7 = 0; i7 < 4;i7++)
+        for (int i8 = 0; i8 < 2;i8++) {
+
+//            Tools::print(numArray);
+            for (int iPos = 0; iPos < pos.size(); iPos++) {
+                vector<int>& onePos = pos.at(iPos);
+                string exp;
+                if (i0 == 1) {
+                    exp.push_back('(');
+                }
+                exp.push_back((char)(cards[onePos[0]] + '0'));
+                exp.push_back(OPERATOR[i1]);
+                if (i2 == 1) {
+                    exp.push_back('(');
+                }
+                exp.push_back((char)(cards[onePos[1]] + '0'));
+                if (i3 == 1) {
+                    exp.push_back(')');
+                }
+                exp.push_back(OPERATOR[i4]);
+                if (i5 == 1) {
+                    exp.push_back('(');
+                }
+                exp.push_back((char)(cards[onePos[2]] + '0'));
+                if (i6 == 1) {
+                    exp.push_back(')');
+                }
+                exp.push_back(OPERATOR[i7]);
+                exp.push_back((char)(cards[onePos[3]] + '0'));
+                if (i8 == 1) {
+                    exp.push_back(')');
+                }
+
+                // handle one expression
+                if (!matchBracket(exp)) {
+//                    cout << exp << endl;
+                    continue;
+                }
+                ChangeInfixToSuffix changeInfixToSuffix;
+                string suffix = changeInfixToSuffix.changeInfixToSuffix(exp);
+                float ret = CalcExp::calcExp(suffix);
+                if ((23.99 <= ret) and (ret <= 24.01)) {
+                    cout << "exp: " << exp << "   suffix: " << suffix << "  ret: " << ret << endl;
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
 public:
     static void testOne(const vector<int>& cards) {
         Tools::print(cards);
